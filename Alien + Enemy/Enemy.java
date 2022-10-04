46b6730d12d9ec1377b24581045dfb8999cfacd8
Putu Mandiasa <113961905+mandiasa02@users.noreply.github.com>
2022-10-04 12:08:58 +0800

    Add files via upload

---
+++ Alien + Enemy/Enemy.java
@@ -0,0 +1,29 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Enemy here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Enemy extends Actor
+{
+    /**
+     * Act - do whatever the Enemy wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public Enemy(){
+        setRotation(180);
+    }
+    public Enemy(int kecepatan){
+        setRotation(180);
+        this.kecepatan = kecepatan;
+    }
+    public int kecepatan = 2;
+    public void act()
+    {
+        // Add your action code here.
+        move(kecepatan);
+    }
+    
+}
